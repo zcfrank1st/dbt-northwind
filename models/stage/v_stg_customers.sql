f060ac6c9c7fdca1ee149699ee13074847f69a6f
zcfrank1st <172180230@qq.com>
2022-05-09 15:44:49 +0800

    change config

---
+++ models/stage/v_stg_customers.sql
@@ -0,0 +1,5 @@
+{{ dbtvault.stage(include_source_columns=true,
+                  source_model=source_model,
+                  derived_columns=derived_columns,
+                  hashed_columns=hashed_columns,
+                  ranked_columns=ranked_columns) }}
